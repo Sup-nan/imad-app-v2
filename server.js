a037669198131f17800925ae5749e26d0172c177
Sup-nan <suprajha.kannan@gmail.com>
2017-03-15 01:35:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-1',function(req,res){
-    res.send("article 1 is requested and served here");
+     res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 app.get('/article-2',function(req,res){
     res.send("article 2 is requested and served here");
